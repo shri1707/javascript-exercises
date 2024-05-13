4077c96e8c86db82b1dc8f8b661c976289e680de
Shrijan Kumar <shri172004@gmail.com>
2024-05-13 13:10:57 +0530

    wrote the function to reverse the string using loops

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(strings) {
+    let a= strings
+    let str=""
+    for (let i=1; i<=a.length;i++){
+        str += a[a.length-i]
+    }
+    return str;
 };
 
 // Do not edit below this line
