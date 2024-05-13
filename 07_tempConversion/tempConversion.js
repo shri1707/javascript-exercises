cdd3ea01df6b2d2f4d6709644b972e0768bcfb52
Shrijan Kumar <shri172004@gmail.com>
2024-05-13 14:58:30 +0530

    written two functions as directed. toFixed(x) method rounds of a number to a specified number of decimals and return a string

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num){
+  C= (num-32) * (5/9)
+  return Number(C.toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  F= (num*(9/5))+32
+  return Number(F.toFixed(1))
 };
 
 // Do not edit below this line
