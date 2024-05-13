f2b0a6f5f5baf345e7abf202251382524053ba86
Shrijan Kumar <shri172004@gmail.com>
2024-05-13 12:48:45 +0530

    written the code to get the expected result and pass the tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+    str = ""
+    if (num < 0) {
+        return "ERROR";
+    }
+    else {
 
+        for (let i = 0; i < num; i++) {
+            str += string
+        }
+        return str
+    }
 };
 
 // Do not edit below this line
