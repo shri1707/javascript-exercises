92fc5f1faf70b89ddfaba6215ef09466877380a5
Shrijan Kumar <shri172004@gmail.com>
2024-05-13 13:43:20 +0530

    we created a function to remove multiple elements from array. We iteratedin reverse order so that SPLICE method will not affect the index of other elements

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...n) {
+    for (let i = arr.length - 1; i >= 0; i--) {
+        if (n.includes(arr[i]))
+            arr.splice(i, 1)
+    }
+    return arr
+}
 
-};
 
 // Do not edit below this line
 module.exports = removeFromArray;
