8cc0c77ed268af9db1d09a774983bba735c4cb48
Shrijan Kumar <shri172004@gmail.com>
2024-05-16 13:06:47 +0530

    used the function given in solution as my function using sort wasn't returning desired results for last test case

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,25 @@
-const findTheOldest = function() {
+// const findTheOldest = function(arr) {
+//     const age = arr.sort((a,b)=> ((a.yearOfDeath-a.yearOfBirth)<(b.yearOfDeath-b.yearOfBirth) ? 1 :-1))
+//     return age[0]
+// };
 
-};
 
+const getAge = function (birth, death) {
+    if (!death) {
+      death = new Date().getFullYear();
+    }
+    return death - birth;
+  };
+  
+  const findTheOldest = function (people) {
+    return people.reduce((oldest, currentPerson) => {
+      const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+      const currentAge = getAge(
+        currentPerson.yearOfBirth,
+        currentPerson.yearOfDeath
+      );
+      return oldestAge < currentAge ? currentPerson : oldest;
+    });
+  };
 // Do not edit below this line
 module.exports = findTheOldest;
