f3cc74dc87b6db6e9a200413470f6d860e0b3235
Shrijan Kumar <shri172004@gmail.com>
2024-05-16 12:20:38 +0530

    wrote the calculator functions and passed all the tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((initial, currentVal) => initial * currentVal, 1)
 };
 
-const power = function() {
-	
+const power = function (a,b) {
+  return a**b
 };
 
-const factorial = function() {
-	
+const factorial = function (x) {
+  let val=1
+  if (x==0 || x==1){
+    return 1
+  }
+  for (let i=1; i<=x; i++){
+    val = val*i
+  }
+  return val
 };
 
 // Do not edit below this line
